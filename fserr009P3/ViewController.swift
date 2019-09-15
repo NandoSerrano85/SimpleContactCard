6899c8adddff27c2fe16dcf7a791a2d257cc284c
Fernando Serrano <fserr009@fiu.edu>
2019-09-15 12:59:53 -0400

    Initial Commit

---
+++ fserr009P3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fserr009P3
+//
+//  Created by Fernando Serrano on 9/15/19.
+//  Copyright © 2019 FIU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
